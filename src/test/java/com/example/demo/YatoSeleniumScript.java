d2d9cd92bf52275262d50dde549c6a601d2a3d8a
yotam <yotam@synopsys.com>
2023-05-06 16:37:36 +0300

    cleaned code and added comments

--- src/test/java/com/example/demo/YatoSeleniumScript.java
+++ src/test/java/com/example/demo/YatoSeleniumScript.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,13 +13,16 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class YatoSeleniumScript {
+    // These fields are defined for the entire class, since all methods need a driver and a driver wait.
     private WebDriver driver;
     private WebDriverWait wait;
 
-
+    /**
+     * The setup method, that runs before each of the methods are executed.
+     * Opens a chromedriver for the driver field, sets a few properties for said driver,
+     * and defines a wait for the wait field.
+     */
     @BeforeEach
     public void setUp()
     {
@@ -35,10 +37,18 @@ public class YatoSeleniumScript {
         wait = new WebDriverWait(driver, 90);
     }
 
+    /**
+     * A method that uses selenium to enter the poll voting page, catch the relevant elements,
+     * send the name 'yato' to all input areas and press the submit button.
+     * @param driver is the reference to the running driver
+     * @param wait is the reference to the defined wait function
+     */
     public void add3VotesForYato(WebDriver driver, WebDriverWait wait)
     {
+        // Enter the voting page
         driver.get("https://www.menti.com/aletmybmwa6w");
 
+        // Catch the relevant elements in the page
         WebElement option1Txt = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(
                 "input[data-testid='vote-input-0']")));
         WebElement option2Txt = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(
@@ -49,6 +59,7 @@ public class YatoSeleniumScript {
         WebElement submitBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(
                 "button[type='submit']")));
 
+        // perform the desired actions on the caught elements
         option1Txt.sendKeys("yato");
         option2Txt.sendKeys("yato");
         option3Txt.sendKeys("yato");
@@ -56,15 +67,26 @@ public class YatoSeleniumScript {
         submitBtn.click();
     }
 
+    /**
+     * This method performs the previous method in a loop, making it possible to vote as many times as desired.
+     */
     @Test
     public void add30VotesForYato()
     {
+        // The loop performs the method 10 times, securing 30 votes for Yato (3 votes per call)
         for (int i=0; i<10; i++)
         {
             add3VotesForYato(driver, wait);
         }
     }
 
+    /**
+     * !This method was not shown in the presentation!
+     * An extra method voting on a different voting form, for a possible 'finals' scene.
+     * It's catching a select box, in which the name 'yato' is chosen and submitted.
+     * @param driver is the reference to the running driver
+     * @param wait is the reference to the defined wait function
+     */
     public void addVoteForYatoInFinals(WebDriver driver, WebDriverWait wait)
     {
         driver.get("https://www.menti.com/aletmybmwa6w");
@@ -79,6 +101,11 @@ public class YatoSeleniumScript {
         submitBtn.click();
     }
 
+    /**
+     * !This method was not shown in the presentation!
+     * This method performs the previous method in a loop, making it possible to vote as many times as desired.
+     * To enable multiple votes, the cookies for the driver are deleted.
+     */
     @Test
     public void add10VotesForYatoInFinals()
     {
@@ -89,28 +116,10 @@ public class YatoSeleniumScript {
         }
     }
 
-    @Test
-    public void basicTestExample()
-    {
-
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
-        String title = driver.getTitle();
-        assertEquals("Web form", title);
-
-        driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
-
-        WebElement textBox = driver.findElement(By.name("my-text"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
-
-        textBox.sendKeys("Selenium");
-        submitButton.click();
-
-        WebElement message = driver.findElement(By.id("message"));
-        String value = message.getText();
-        assertEquals("Received!", value);
-    }
-
+    /**
+     * The tear down method, that runs after every method finishes its execution.
+     * Removes the driver after its job is done.
+     */
     @AfterEach
     public void tearDown()
     {
@@ -119,26 +128,3 @@ public class YatoSeleniumScript {
     }
 
 }
-
-// extra code for reference
-/*
-System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver");
-ChromeDriver chromeDriver = new ChromeDriver(createChromeOptions());
-
-chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
-chromeDriver.manage().timeouts().scriptTimeout(Duration.ofSeconds(60));
-chromeDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
-
-chromeDriver.manage().window().maximize();
-
-
-@Test
-WebDriver driver = this.basePage.driver;
-WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(90));
-
-
-
-
-driver.close();
-driver.quit();
- */
